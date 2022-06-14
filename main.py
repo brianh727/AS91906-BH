0958bfd5e076661d990f17c533a5de748d82c9d2
school <brian.huang@my.bdsc.school.nz>
2022-06-15 10:33:29 +1200

    creating returning for the login menu

--- main.py
+++ main.py
@@ -15,14 +15,17 @@ class Main(QMainWindow):
         super(Main, self).__init__(parent)
         self.setWindowTitle("FastCards")
         self.widgets = QStackedWidget()
-        start_widget = Start(self)
-        self.widgets.addWidget(start_widget)
+        self.start_widget = Start(self)
+        self.widgets.addWidget(self.start_widget)
         self.setCentralWidget(self.widgets)
+
+    def main_menu(self):
+        self.widgets.setCurrentWidget(self.start_widget)
     
-    def login(self):
-        login_widget = Login(self)
-        self.widgets.addWidget(login_widget)
-        self.widgets.setCurrentWidget(login_widget)
+    def login_menu(self):
+        self.login_widget = Login(self)
+        self.widgets.addWidget(self.login_widget)
+        self.widgets.setCurrentWidget(self.login_widget)
 
 
 class Start(QWidget):
@@ -40,20 +43,38 @@ class Start(QWidget):
         self.start_layout.addWidget(self.signup_button)
         self.start_layout.addWidget(self.guest_button)
 
-        self.login_button.clicked.connect(self.parent().login)
+        self.login_button.clicked.connect(self.parent().login_menu)
         
 
 class Login(QWidget):
-    """Login menu for the user to log in"""
+    """Login menu for the user to log in to an existing account"""
     def __init__(self, parent=None):
         super(Login, self).__init__(parent)
         self.login_layout = QVBoxLayout()
         self.setLayout(self.login_layout)
 
-        self.title = QLineEdit("Log into your account:")
+        self.title = QLabel("Log into your account:")
+        self.user_line = QLineEdit()
+        self.user_line.setPlaceholderText("Username")
+        self.pass_line = QLineEdit()
+        self.pass_line.setPlaceholderText("Password")
+        self.login_button = QPushButton("Login")
+        self.return_button = QPushButton("Return")
+
+        self.login_button.clicked.connect(self.login_check)
+        self.return_button.clicked.connect(self.parent().main_menu)
 
         self.login_layout.addWidget(self.title)
+        self.login_layout.addWidget(self.user_line)
+        self.login_layout.addWidget(self.pass_line)
 
+        self.buttons_layout = QHBoxLayout()
+        self.login_layout.addLayout(self.buttons_layout)
+        self.buttons_layout.addWidget(self.login_button)
+        self.buttons_layout.addWidget(self.return_button)
+
+    def login_check(self):
+        pass
 
 
 def main():
