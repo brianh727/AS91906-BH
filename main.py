4d3e97456b9fcffc173d8866204570e3bde88b22
school <brian.huang@my.bdsc.school.nz>
2022-06-15 09:40:50 +1200

    created switching menus into login

--- main.py
+++ main.py
@@ -5,22 +5,30 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import (QAction, QApplication, QHBoxLayout, QLabel, 
                              QMainWindow, QToolBar, QVBoxLayout, QWidget,
                              QTabWidget, QTableWidget, QAbstractItemView,
-                             QPushButton, QGridLayout, QLineEdit)
+                             QPushButton, QGridLayout, QLineEdit, QStackedWidget)
 
 
 class Main(QMainWindow):
-    """Create the main window for all the widgets"""
-
-    def __init__(self):
+    """The main window for all the menu widgets of each section"""
+    def __init__(self, parent=None):
         """Initialize the settings of the main window"""
-        super().__init__()
+        super(Main, self).__init__(parent)
         self.setWindowTitle("FastCards")
-        self.setCentralWidget(Start())
+        self.widgets = QStackedWidget()
+        start_widget = Start(self)
+        self.widgets.addWidget(start_widget)
+        self.setCentralWidget(self.widgets)
+    
+    def login(self):
+        login_widget = Login(self)
+        self.widgets.addWidget(login_widget)
+        self.widgets.setCurrentWidget(login_widget)
 
 
 class Start(QWidget):
-    def __init__(self):
-        super().__init__()
+    """Start menu for the login buttons"""
+    def __init__(self, parent=None):
+        super(Start, self).__init__(parent)
         self.start_layout = QVBoxLayout()
         self.setLayout(self.start_layout)
         
@@ -31,6 +39,21 @@ class Start(QWidget):
         self.start_layout.addWidget(self.login_button)
         self.start_layout.addWidget(self.signup_button)
         self.start_layout.addWidget(self.guest_button)
+
+        self.login_button.clicked.connect(self.parent().login)
+        
+
+class Login(QWidget):
+    """Login menu for the user to log in"""
+    def __init__(self, parent=None):
+        super(Login, self).__init__(parent)
+        self.login_layout = QVBoxLayout()
+        self.setLayout(self.login_layout)
+
+        self.title = QLineEdit("Log into your account:")
+
+        self.login_layout.addWidget(self.title)
+
 
 
 def main():
